2cad70820a5a34c79e925339acb60a7be057771b
Ometia <tethatol@gmail.com>
2022-04-11 21:15:08 -0600

    Initial Commit
    Initial commit, just to get things rolling.

---
+++ src/main/java/xyz/ometia/psychocads/item/ModItems.java
@@ -0,0 +1,17 @@
+package xyz.ometia.psychocads.item;
+
+
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+
+@EventBusSubscriber(bus = Bus.MOD)
+public class ModItems {
+    public static void init(RegistryEvent.Register<Item> event) {
+        event.getRegistry().registerAll(new ());
+
+    }
+
+}
---
+++ src/main/java/xyz/ometia/psychocads/item/abyssalCAD.java
@@ -0,0 +1,28 @@
+package xyz.ometia.psychocads.item;
+
+import xyz.ometia.psychocads.PsychoCADs;
+import java.util.List;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import vazkii.psi.api.PsiAPI;
+import vazkii.psi.api.cad.EnumCADComponent;
+import vazkii.psi.api.cad.EnumCADStat;
+import vazkii.psi.api.cad.ICAD;
+import vazkii.psi.api.cad.ICADComponent;
+
+@EventBusSubscriber
+public class abyssalCAD extends Item implements ICADComponent{
+}
